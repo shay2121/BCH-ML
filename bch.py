72ebbeb44a9d436ffd1c2e086bf9006dacaca511
shay <tabibis@gmail.com>
2016-11-18 11:23:23 +0200

    basic bch3x7

--- bch.py
+++ bch.py
@@ -1,56 +1,72 @@
-import random as random
-import numpy as np
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import codes
 import csv
 
+
+import tensorflow as tf
+import numpy as np
 
 globalK=3
 globalN=7
 globalM=globalN-globalK
-snr=3
+globalSnr=3
+globalInd=2
 numOfWords=100000
 
 
-dataFileName='Data/bch '+str(globalK)+'x'+str(globalN)+'.csv'
-dataFile = open(dataFileName, "wb")
-dataWriter = csv.writer(dataFile, delimiter='	', quotechar='"', quoting=csv.QUOTE_ALL)
-
-
-
-
-def readCsvG():
-  fname='CFG/codes.csv'
-  t = np.genfromtxt(fname, delimiter=',')
-  r=t[1:]
-  return r
-
-
-
-
-G=readCsvG();
-
-def flip(p):
-    return 0 if random.random() < 0.5 else 1
-
-def generateCodeWord():
-  R=np.true_divide(globalK,globalN)
-  snrUp=snr+10*np.log10(2.0*R)
-  factor = 2 * pow(10.0, (snrUp / 20.0))
-  noise=np.random.normal(0,1,globalN)/factor
-  w= np.random.randint(2, size=globalK)
-  y=np.remainder(np.dot(w,G),2)
-  x=y+noise
-  dataWriter.writerow(y)
-  dataWriter.writerow(x)
-  return x
+def genData():
+  codes.generateData(globalK, globalN, globalSnr, numOfWords,globalInd)
+ # fname='Data/bch' + str(globalK) + 'x' + str(globalN) + '.csv'
+  #t = np.genfromtxt(fname, delimiter=',')
+  #return t
 
 
 def main():
-    print("- In the forest:")
-    for i in range(1,numOfWords):
-        generateCodeWord()
-        if np.remainder(i,1000)==0:
-            print(str(i/1000)+"%")
-    print("end")
+#    codes.generateData(globalK,globalN,globalSnr,numOfWords)
+#    r=genData()
+    print("start train...")
 
 
 if __name__ == "__main__": main()
+
+
+# Data sets
+BCH_TRAINING= 'BCH3x7train.csv'
+BCH_TEST = 'BCH3x7test.csv'
+
+# Load datasets.
+
+training_set = tf.contrib.learn.datasets.base.load_csv(filename=BCH_TRAINING, target_dtype=np.int)
+test_set = tf.contrib.learn.datasets.base.load_csv(filename=BCH_TEST, target_dtype=np.int)
+
+
+
+# Specify that all features have real-value data
+feature_columns = [tf.contrib.layers.real_valued_column("", dimension=7)]
+
+# Build 3 layer DNN with 10, 20, 10 units respectively.
+classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                            hidden_units=[10, 20, 10],
+                                            n_classes=3,
+                                            model_dir="/tmp/bch_model")
+
+# Fit model.
+classifier.fit(x=training_set.data,
+               y=training_set.target,
+               steps=2000)
+
+# Evaluate accuracy.
+accuracy_score = classifier.evaluate(x=test_set.data,
+                                     y=test_set.target)["accuracy"]
+print('Accuracy: {0:f}'.format(accuracy_score))
+
+print("game on")
+# Classify two new flower samples.
+#  new_samples = np.array(
+#     [[6.4, 3.2, 4.5, 1.5], [5.8, 3.1, 5.0, 1.7]], dtype=float)
+#y = classifier.predict(new_samples)
+#print('Predictions: {}'.format(str(y)))
